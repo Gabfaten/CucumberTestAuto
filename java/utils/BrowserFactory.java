8b03bbce113ac52c07655c779ce51b34897081a5
Gabfaten <148710713+Gabfaten@users.noreply.github.com>
2023-11-16 19:26:56 +0100

    Update BrowserFactory.java

--- java/utils/BrowserFactory.java
+++ java/utils/BrowserFactory.java
@@ -1,20 +1,36 @@
 package utils;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactory {
-	//Todo:configure Browsers	
-	
+
 	static WebDriver factoryDriver;	
-	String webBrowserType = "Chrome";
 	
+	public static final HashMap<String, String> configurationMap = 
+			PropertiesFile.read("src/test/resources/environment/config.properties");
+	
+	static String webBrowserType = configurationMap.get("browser");	
+	
+
 	public static WebDriver getFactoryDriver() {
-		//chrome
-		 System.getProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
-		 factoryDriver = new ChromeDriver();	
-		return factoryDriver;
+			
+	  if(webBrowserType.equalsIgnoreCase("chrome") ) {
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\SAMSUNG\\eclipse-workspace\\TestAuto\\src\\test\\resources\\drivers\\chromedriver.exe");
+		 factoryDriver = new ChromeDriver();
+	}
+	   else if (webBrowserType.equalsIgnoreCase("firefox") ) {
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\SAMSUNG\\eclipse-workspace\\TestAuto\\src\\test\\resources\\drivers\\geckodriver.exe");				
+			factoryDriver =  new FirefoxDriver();
+		}	
+	  
+	  return factoryDriver;
+		
 	}
 	
 
 }
+
